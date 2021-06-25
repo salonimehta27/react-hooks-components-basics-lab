36061d593852c14577f9d1f180cbc68316cf0846
Saloni <salonimehta2127@gmail.com>
2021-06-24 22:24:15 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,13 +15,19 @@ function Home() {
     </div>
   );
 }
-
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      {<Home />}
+      { <About />}
     </div>
   );
 }
